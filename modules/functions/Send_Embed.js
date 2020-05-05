e38ff90eed3dc95a9a8d403f0ca42aab121033cd
Your Mother <r.g.levasseur@gmail.com>
2020-05-04 21:58:45 -0400

    Restart if 5 channel send errors

--- modules/functions/Send_Embed.js
+++ modules/functions/Send_Embed.js
@@ -6,7 +6,10 @@ module.exports = (MAIN, type, raid_level, server, content, embed, channel_id) =>
   if(!MAIN.Next_Bot){ MAIN.Next_Bot = 0; }
   if(MAIN.Next_Bot == MAIN.BOTS.length-1 && MAIN.BOTS[0]){ MAIN.Next_Bot = 0; } else{ MAIN.Next_Bot++; }
   let channel = MAIN.BOTS[MAIN.Next_Bot].channels.cache.get(channel_id);
-  if(!channel) { return console.error('Problem finding channel: '+channel_id+' using Bot: '+MAIN.Next_Bot); }
+  if(!channel) {
+    errors++; console.error('Problem finding channel: '+channel_id+' using Bot: '+MAIN.Next_Bot);
+    if(errors >= 5){ MAIN.restart('Channel Send Errors',1); }
+  }
 	return channel.send(content, embed).catch( error => {
     return console.error('[Send_Embed] ['+MAIN.Bot_Time(null,'stamp')+'] ['+channel_id+']', error);
   });
