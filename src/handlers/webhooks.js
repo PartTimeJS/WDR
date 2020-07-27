28f392001c74504c21b56edab003c4153ea49a44
Your Mother <r.g.levasseur@gmail.com>
2020-07-27 11:27:57 -0400

    change variable

--- src/handlers/webhooks.js
+++ src/handlers/webhooks.js
@@ -1,148 +1,124 @@
-module.exports = (WDR, Payload) => {
+module.exports = async (WDR, Payload) => {
 
-  Payload.forEach(data => {
+  for (let p = 0, plen = Payload.length; p < plen; p++) {
+    let data = Payload[p];
+    //Payload.forEach(async data => {
 
-    // LOOP DISCORDS TO MATCH A GEOFENCE
-    WDR.Discords.forEach(async discord => {
+    for (let d = 0, dlen = WDR.Discords.length; d < dlen; d++) {
+      let discord = WDR.Discords[d];
 
-      let payload = {};
+      let object = data.message;
 
-      // ASSIGN DISCORD TO VARIABLE
-      data.message.Discord = discord;
+      object.Discord = discord;
 
-      // CHECK GEOFENCES TO FIND A MATCH
-      if (WDR.PointInGeoJSON.polygon(data.message.Discord.geofence, [data.message.longitude, data.message.latitude])) {
+      if (WDR.PointInGeoJSON.polygon(object.Discord.geofence, [object.longitude, object.latitude])) {
 
-        // GET TIME
-        data.message.Time_Now = new Date().getTime();
+        object.Time_Now = new Date().getTime();
 
-        // DEFINE AND DETERMINE TIMEZONE
-        data.message.Timezone = WDR.GeoTz(data.message.Discord.geofence[0][1][1], data.message.Discord.geofence[0][1][0])[0];
+        object.Timezone = WDR.GeoTz(object.Discord.geofence[0][1][1], object.Discord.geofence[0][1][0])[0];
 
-        // DEFINE AREAS FROM GEOFENCE FILE
-        data.message.area = {};
-        data.message.area.default = data.message.Discord.name;
+        object.area = {};
+        object.area.default = object.Discord.name;
 
-        // CHECK IF GEOJSON EXISTS
-        if (data.message.Discord.geojson_file && data.message.Discord.geojson_file != "") {
-          data.message.area = await WDR.Get_Areas(WDR, data.message);
+        if (object.Discord.geojson_file && object.Discord.geojson_file != "") {
+          object.area = await WDR.Get_Areas(WDR, object);
         }
 
-        // ASSIGN AREA TO VARIABLES
-        if (data.message.area.sub) {
-          data.message.area.embed = data.message.area.sub;
-        } else if (data.message.area.main && !data.message.area.sub) {
-          data.message.area.embed = data.message.area.main;
-        } else if (!data.message.area.sub && !data.message.area.main) {
-          data.message.area.embed = data.message.area.default;
+        if (object.area.sub) {
+          object.area.embed = object.area.sub;
+        } else if (object.area.main && !object.area.sub) {
+          object.area.embed = object.area.main;
+        } else if (!object.area.sub && !object.area.main) {
+          object.area.embed = object.area.default;
         }
 
-        // GET RECEIVE TIME FOR PROCESSING TIME
         if (WDR.Config.DEBUG.Processing_Speed == "ENABLED") {
-          data.message.WDR_Received = new Date().getTime();
+          object.WDR_Received = new Date().getTime();
         }
 
-        // SEND TO OBJECT MODULES
         if (data.type == "pokemon") {
 
-          if (data.message.cp > 0) {
+          if (object.cp > 0) {
 
-            data.message.gen = await WDR.Get_Gen(data.message.pokemon_id);
+            object.gen = await WDR.Get_Gen(object.pokemon_id);
 
-            data.message.weather_boost = await WDR.Get_Weather(WDR, data.message);
-            if (data.message.weather_boost == undefined) {
-              WDR.Console.error(WDR, "[handlers/webhooks.js] Undefined Emoji for Weather ID " + data.message.weather + ". Emoji does not exist in defined emoji server(s).");
+            object.weather_boost = await WDR.Get_Weather(WDR, object);
+            if (object.weather_boost == undefined) {
+              WDR.Console.error(WDR, "[handlers/webhooks.js] Undefined Emoji for Weather ID " + object.weather + ". Emoji does not exist in defined emoji server(s).");
             }
 
-            data.message.size = await WDR.Get_Size(WDR, data.message.pokemon_id, data.message.form, data.message.height, data.message.weight);
+            object.size = await WDR.Get_Size(WDR, object.pokemon_id, object.form, object.height, object.weight);
 
-            data.message = await WDR.Get_Locale.Pokemon(WDR, data.message);
+            object = await WDR.Get_Locale.Pokemon(WDR, object);
 
-            data.message.internal_value = (Math.floor(((data.message.individual_defense + data.message.individual_stamina + data.message.individual_attack) / 45) * 1000) / 10);
+            object.internal_value = (Math.floor(((object.individual_defense + object.individual_stamina + object.individual_attack) / 45) * 1000) / 10);
 
-            if (data.message.gender == 1) {
-              data.message.gender_name = "male";
-              data.message.gender_id = 1;
-            } else if (data.message.gender == 2) {
-              data.message.gender_name = "female";
-              data.message.gender_id = 2;
+            if (object.gender == 1) {
+              object.gender_name = "male";
+              object.gender_id = 1;
+            } else if (object.gender == 2) {
+              object.gender_name = "female";
+              object.gender_id = 2;
             } else {
-              delete data.message.gender;
-              data.message.gender_name = "all";
-              data.message.gender_id = 0;
+              delete object.gender;
+              object.gender_name = "all";
+              object.gender_id = 0;
             }
-            if (data.message.gender) {
-              data.message.gender_wemoji = await WDR.Capitalize(data.message.gender_name) + " " + WDR.Emotes[data.message.gender_name];
-              data.message.gender_noemoji = await WDR.Capitalize(data.message.gender_name);
+            if (object.gender) {
+              object.gender_wemoji = await WDR.Capitalize(object.gender_name) + " " + WDR.Emotes[object.gender_name];
+              object.gender_noemoji = await WDR.Capitalize(object.gender_name);
             }
 
-            // SEND TO POKEMON SUBSCRIPTION PROCESSING
-            WDR.Subscriptions.Pokemon(WDR, data.message);
+            WDR.Subscriptions.Pokemon(WDR, object);
 
-            // SEND TO POKEMON FEED PROCESSING
-            WDR.Feeds.Pokemon(WDR, data.message);
+            WDR.Feeds.Pokemon(WDR, object);
 
-            // GET GREAT LEAGUE STATS
-            data.message.great_league = await WDR.PvP.CalculatePossibleCPs(WDR, data.message.pokemon_id, data.message.form_id, data.message.individual_attack, data.message.individual_defense, data.message.individual_stamina, data.message.pokemon_level, data.message.gender_name, "great", "webhook.js great");
+            object.great_league = await WDR.PvP.CalculatePossibleCPs(WDR, object.pokemon_id, object.form_id, object.individual_attack, object.individual_defense, object.individual_stamina, object.pokemon_level, object.gender_name, "great", "webhook.js great");
 
-            // GET ULTRA LEAGUE STATS
-            data.message.ultra_league = await WDR.PvP.CalculatePossibleCPs(WDR, data.message.pokemon_id, data.message.form_id, data.message.individual_attack, data.message.individual_defense, data.message.individual_stamina, data.message.pokemon_level, data.message.gender_name, "ultra", "webhook.js ultra");
+            object.ultra_league = await WDR.PvP.CalculatePossibleCPs(WDR, object.pokemon_id, object.form_id, object.individual_attack, object.individual_defense, object.individual_stamina, object.pokemon_level, object.gender_name, "ultra", "webhook.js ultra");
 
-            // SEND TO POKEMON SUBSCRIPTION FILTERING
-            WDR.Subscriptions.PvP(WDR, data.message);
+            WDR.Subscriptions.PvP(WDR, object);
 
-            // SEND TO POKEMON FEED FILTERING
-            WDR.Feeds.PvP(WDR, data.message);
+            WDR.Feeds.PvP(WDR, object);
           } else {
 
-            // SEND TO NO IV FEED
-            //WDR.Feeds.NoIVPokemon(WDR, data.message);
-            //WDR.Subscriptions.NoIVPokemon(WDR, data.message);
+            //WDR.Feeds.NoIVPokemon(WDR, object);
+            //WDR.Subscriptions.NoIVPokemon(WDR, object);
           }
 
         } else if (data.type == "raid") {
 
-          data.message = await WDR.Get_Locale.Pokemon(WDR, data.message);
+          object = await WDR.Get_Locale.Pokemon(WDR, object);
 
-          // SEND TO RAID FEED FILTERING
-          WDR.Feeds.Raids(WDR, data.message);
+          WDR.Feeds.Raids(WDR, object);
 
-          // SEND TO RAID SUBSCRIPTION FILTERING
-          //WDR.Subscriptions.Raids(WDR, data.message);
+          //WDR.Subscriptions.Raids(WDR, object);
 
         } else if (data.type == "quest") {
 
-          // GET QUEST REWARD
-          data.message = await WDR.Get_Quest_Reward(WDR, data.message);
+          object = await WDR.Get_Quest_Reward(WDR, object);
 
-          // GET QUEST TASK
-          data.message = await WDR.Get_Quest_Task(WDR, data.message);
+          object = await WDR.Get_Quest_Task(WDR, object);
 
-          // SEND TO QUEST FEED FILTERING
-          WDR.Feeds.Quests(WDR, data.message);
+          WDR.Feeds.Quests(WDR, object);
 
-          // SEND TO QUEST SUBSCRIPTION FILTERING
-          //WDR.Subscriptions.Quests(WDR, data.message);
+          //WDR.Subscriptions.Quests(WDR, object);
 
         } else if (data.type == "pokestop") {
 
-          // SEND TO LURE FEED FILTERING
-          WDR.Feeds.Lures(WDR, data.message);
+          WDR.Feeds.Lures(WDR, object);
 
-          // SEND TO LURE SUBSCRIPTION FILTERING
-          //WDR.Subscriptions.Lures(WDR, data.message);
+          //WDR.Subscriptions.Lures(WDR, object);
 
         } else if (data.type == "invasion") {
 
-          // SEND TO INVASION FEED FILTERING
-          WDR.Feeds.Invasions(WDR, data.message);
+          WDR.Feeds.Invasions(WDR, object);
 
-          // SEND TO INVASION SUBSCRIPTION FILTERING
-          //WDR.Subscriptions.Invasions(WDR, data.message);
+          //WDR.Subscriptions.Invasions(WDR, object);
         }
       }
-    });
-  });
+    }
+  } //);
 
   // END
   return;
@@ -154,11 +130,11 @@ function Calculate_Size(pokemon_id) {
   if (form_id > 0) {
     let form_weight = WDR.Master.Pokemon[pokemon_id].forms[form_id].weight ? WDR.Master.Pokemon[pokemon_id].forms[form_id].weight : WDR.Master.Pokemon[pokemon_id].weight;
     let form_height = WDR.Master.Pokemon[pokemon_id].forms[form_id].height ? WDR.Master.Pokemon[pokemon_id].forms[form_id].height : WDR.Master.Pokemon[pokemon_id].height;
-    weightRatio = data.message.weight / form_weight;
-    heightRatio = data.message.height / form_height;
+    weightRatio = object.weight / form_weight;
+    heightRatio = object.height / form_height;
   } else {
-    weightRatio = data.message.weight / WDR.Master.Pokemon[pokemon_id].weight;
-    heightRatio = data.message.height / WDR.Master.Pokemon[pokemon_id].height;
+    weightRatio = object.weight / WDR.Master.Pokemon[pokemon_id].weight;
+    heightRatio = object.height / WDR.Master.Pokemon[pokemon_id].height;
   }
 
   let size = heightRatio + weightRatio;
