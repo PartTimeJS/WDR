8bfab15c2d5f96165d44965cc1dd778f53cf7b89
Your Mother <r.g.levasseur@gmail.com>
2020-07-27 08:40:41 -0400

    Situational Customization

--- src/commands/subscription/cancel.js
+++ src/commands/subscription/cancel.js
@@ -1,9 +1,19 @@
-module.exports = (WDR, Functions, Message, Member) => {
-  let subscription_cancel = new WDR.DiscordJS.MessageEmbed().setColor("00ff00")
-    .setAuthor(Member.db.user_name, Member.user.displayAvatarURL())
-    .setTitle("Subscription Cancelled.")
-    .setDescription("Nothing has been Saved.")
-  return Message.channel.send(subscription_cancel).then(m => m.delete({
-    timeout: 5000
-  })).catch(console.error);
+module.exports = (WDR, Functions, Message, Member, type) => {
+  if (!type) {
+    let subscription_cancel = new WDR.DiscordJS.MessageEmbed().setColor("00ff00")
+      .setAuthor(Member.db.user_name, Member.user.displayAvatarURL())
+      .setTitle("Subscription Cancelled.")
+      .setDescription("Nothing has been Saved.")
+    return Message.channel.send(subscription_cancel).then(m => m.delete({
+      timeout: 5000
+    })).catch(console.error);
+  } else {
+    let subscription_cancel = new WDR.DiscordJS.MessageEmbed().setColor("00ff00")
+      .setAuthor(Member.db.user_name, Member.user.displayAvatarURL())
+      .setTitle(type + " Subscription Cancelled.")
+      .setDescription("Nothing has been Saved.")
+    return Message.channel.send(subscription_cancel).then(m => m.delete({
+      timeout: 5000
+    })).catch(console.error);
+  }
 }
--- src/commands/subscription/timedout.js
+++ src/commands/subscription/timedout.js
@@ -1,9 +1,19 @@
-module.exports = (WDR, Functions, Message, Member) => {
-  let subscription_cancel = new WDR.DiscordJS.MessageEmbed().setColor("00ff00")
-    .setAuthor(Member.db.user_name, Member.user.displayAvatarURL())
-    .setTitle("Your Subscription Has Timed Out.")
-    .setDescription("Nothing has been Saved.");
-  return Message.channel.send(subscription_cancel).then(m => m.delete({
-    timeout: 5000
-  })).catch(console.error);
+module.exports = (WDR, Functions, Message, Member, type) => {
+  if (!type) {
+    let subscription_cancel = new WDR.DiscordJS.MessageEmbed().setColor("00ff00")
+      .setAuthor(Member.db.user_name, Member.user.displayAvatarURL())
+      .setTitle("Your Subscription Has Timed Out.")
+      .setDescription("Nothing has been Saved.");
+    return Message.channel.send(subscription_cancel).then(m => m.delete({
+      timeout: 5000
+    })).catch(console.error);
+  } else {
+    let subscription_cancel = new WDR.DiscordJS.MessageEmbed().setColor("00ff00")
+      .setAuthor(Member.db.user_name, Member.user.displayAvatarURL())
+      .setTitle("Your " + type + " Subscription Has Timed Out.")
+      .setDescription("Nothing has been Saved.");
+    return Message.channel.send(subscription_cancel).then(m => m.delete({
+      timeout: 5000
+    })).catch(console.error);
+  }
 }
