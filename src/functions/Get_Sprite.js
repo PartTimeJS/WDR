e721fc5e916d84bc804d0be1249ffb3659d8c1ef
Your Mother <r.g.levasseur@gmail.com>
2020-08-19 22:58:38 -0400

    sprite change to use id_formid

--- src/functions/Get_Sprite.js
+++ src/functions/Get_Sprite.js
@@ -1,55 +1,65 @@
 // OBTAIN POKEMON SPRITE
 module.exports = (WDR, Object, type) => {
+  let sprite = null;
   let sprite_url = WDR.Config.SPRITE_URL;
-  let extension = "";
 
-  switch (true) {
+  if (Object.pokemon_id > 0) {
+    let pokemon_id = "",
+      form_id = "",
+      costume_id = "";
 
-    // ASSET ICONS
-    case (Object.pokemon_id > 0):
-      if (Object.form > 0) {
-        if (WDR.Master.Pokemon[Object.pokemon_id].forms[Object.form] && WDR.Master.Pokemon[Object.pokemon_id].forms[Object.form].asset_form) {
-          extension = "_" + WDR.Master.Pokemon[Object.pokemon_id].forms[Object.form].asset_form;
-        } else {
-          extension = "_00" + extension;
-        }
+    pokemon_id = pad(Object.pokemon_id, 3);
+
+    if (Object.form > 0) {
+      if (WDR.Master.Pokemon[Object.pokemon_id].forms[Object.form] && WDR.Master.Pokemon[Object.pokemon_id].forms[Object.form].form_id) {
+        form_id = "_" + WDR.Master.Pokemon[Object.pokemon_id].forms[Object.form].form_id;
       } else {
-        extension = "_00" + extension;
+        form_id = "_00";
       }
-      if (Object.costume && Object.costume > 0) {
-        if (Object.costume < 10) {
-          extension = extension + "_0" + Object.costume;
-        } else {
-          extension = extension + "_" + Object.costume;
-        }
-      }
-      return (WDR.Config.SPRITE_URL + "pokemon_icon_" + pad(Object.pokemon_id, 3) + extension + ".png");
-      break;
+    } else {
+      form_id = "_00";
+    }
 
-    case (Object.rewards && Object.rewards[0] && Object.rewards[0].type):
-      WDR.Console.error(WDR, "GETTING A REWARD SPRITE");
-      switch (Object.rewards[0].type) {
-        case 1:
-          return null;
+    if (Object.costume && Object.costume > 0) {
+      costume_id = pad(Object.costume, 2);
+    }
 
-        case 2:
-          return "https://raw.githubusercontent.com/PartTimeJS/Assets/master/pogo/items/item_" + Object.rewards[0].info.item_id + ".png";
+    sprite = WDR.Config.SPRITE_URL + "pokemon_icon_" + pokemon_id + form_id + costume_id + ".png";
 
-        case 3:
-          return "https://raw.githubusercontent.com/PartTimeJS/Assets/master/pogo/items/stardust.png";
+  } else if (Object.rewards.length > 0) {
+    switch (Object.rewards[0].type) {
+      case 1:
+        sprite = null;
+        break;
 
-        case 4:
-          return "https://raw.githubusercontent.com/PartTimeJS/Assets/master/pogo/items/Item_1301.png";
+      case 2:
+        let item_id = pad(Object.rewards[0].info.item_id, 4);
+        sprite = "https://raw.githubusercontent.com/PartTimeJS/Assets/master/pogo/items/item_" + item_id + ".png";
+        break;
 
-        case 5:
-          return null;
+      case 3:
+        sprite = "https://raw.githubusercontent.com/PartTimeJS/Assets/master/pogo/items/stardust.png";
+        break;
 
-        case 6:
-          return null;
-      }
-      break;
+      case 4:
+        sprite = "https://raw.githubusercontent.com/PartTimeJS/Assets/master/pogo/items/Item_1301.png";
+        break;
+
+      case 5:
+        sprite = null;
+        break;
+
+      case 6:
+        sprite = null;
+        break;
+    }
   }
-  return null;
+
+  if (sprite == null) {
+    console.error("NULL SPRITE ", Object);
+  }
+
+  return sprite;
 }
 
 function pad(num, size) {
