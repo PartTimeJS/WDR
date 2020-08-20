6dcfd6bf5f618211f9cc2e4b15fdf97488a2ca6f
Your Mother <r.g.levasseur@gmail.com>
2020-08-19 22:56:45 -0400

    dm condition

--- src/functions/Send_DM.js
+++ src/functions/Send_DM.js
@@ -3,13 +3,26 @@ module.exports = (WDR, guild_id, user_id, embed, bot) => {
   if (!WDR.Bot.Array[bot]) {
     bot = 0;
   }
-  WDR.Bot.Array[bot].guilds.cache.get(guild_id).members.fetch(user_id).then(TARGET => {
-    return TARGET.send(embed).catch(error => {
-      if (error.code == 'ECONNRESET') {
-        return console.error('[Send_DM] [' + WDR.Time(null, 'stamp') + '] [' + user_id + '] Error Code ', error.code);
-      } else {
-        return console.error('[Send_DM] [' + WDR.Time(null, 'stamp') + '] [' + user_id + '] [' + WDR.Bot.Array[bot].id + '] ', error);
-      }
+  if (bot) {
+    WDR.Bot.Array[bot].users.fetch(user_id).then(TARGET => {
+      return TARGET.send(embed).catch(error => {
+        if (error.code == 'ECONNRESET') {
+          return console.error('[Send_DM] [' + WDR.Time(null, 'stamp') + '] [' + user_id + '] Error Code ', error.code);
+        } else {
+          return console.error('[Send_DM] [' + WDR.Time(null, 'stamp') + '] [' + user_id + '] [' + WDR.Bot.Array[bot].id + '] ', error);
+        }
+      });
     });
-  });
+  } else {
+    WDR.Bot.users.fetch(user_id).then(TARGET => {
+      return TARGET.send(embed).catch(error => {
+        console.log(error);
+        if (error.code == 'ECONNRESET') {
+          return console.error('[Send_DM] [' + WDR.Time(null, 'stamp') + '] [' + user_id + '] Error Code ', error.code);
+        } else {
+          return console.error('[Send_DM] [' + WDR.Time(null, 'stamp') + '] [' + user_id + '] [' + WDR.Bot.Array[bot].id + '] ', error);
+        }
+      });
+    });
+  }
 }
