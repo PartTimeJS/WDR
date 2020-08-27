ed0891b85a3e6594fe8ff36665def2a2f0b49dd3
Your Mother <r.g.levasseur@gmail.com>
2020-08-26 22:40:48 -0400

    quest feed fixes

--- src/feeds/quests.js
+++ src/feeds/quests.js
@@ -7,55 +7,107 @@ module.exports = async (WDR, Quest) => {
     let feed_channel = WDR.Quest_Channels[c];
 
     // LOOK UP CHANNEL
-    let Channel = WDR.Bot.channels.cache.get(feed_channel[0]);
-    if (!Channel) {
-      return WDR.Console.error(WDR, "[feeds/raids.js] The channel " + feed_channel[0] + " does not appear to exist.");
+    let channel = WDR.Bot.channels.cache.get(feed_channel[0]);
+    if (!channel) {
+      return WDR.Console.error(WDR, "[feeds/quests.js] The channel " + feed_channel[0] + " does not appear to exist.");
     }
 
     // FETCH CHANNEL GEOFENCE
-    Channel.Geofences = feed_channel[1].geofences.split(",");
-    if (!Channel.Geofences) {
-      return WDR.Console.error(WDR, "[feeds/raids.js] You do not have a Geofences set for " + feed_channel[1] + ".");
+    channel.geofences = feed_channel[1].geofences.split(",");
+    if (!channel.geofences) {
+      return WDR.Console.error(WDR, "[feeds/quests.js] You do not have a Geofences set for " + feed_channel[1] + ".");
     }
 
     // FETCH CHANNEL FILTER
-    Channel.Filter = WDR.Filters.get(feed_channel[1].filter);
-    if (!Channel.Filter) {
-      return WDR.Console.error(WDR, "[feeds/raids.js] The filter defined for " + feed_channel[0] + " does not appear to exist.");
+    channel.Filter = WDR.Filters.get(feed_channel[1].filter);
+    if (!channel.Filter) {
+      return WDR.Console.error(WDR, "[feeds/quests.js] The filter defined for " + feed_channel[0] + " does not appear to exist.");
     }
 
     // CHECK CHANNEL FILTER TYPE
-    if (Channel.Filter.Type != "quest") {
-      return WDR.Console.error(WDR, "[feeds/raids.js] The filter defined for " + feed_channel[0] + " does not appear to be a quest filter.");
+    if (channel.Filter.Type != "quest") {
+      return WDR.Console.error(WDR, "[feeds/quests.js] The filter defined for " + feed_channel[0] + " does not appear to be a quest filter.");
     }
 
     // ADD ROLE ID IF IT EXISTS IN CHANNEL CONFIG
     if (feed_channel[1].roleid) {
       if (feed_channel[1].roleid == "here" || feed_channel[1].roleid == "everyone") {
-        Raid.role_id = "@" + feed_channel[1].roleid;
+        Quest.role_id = "@" + feed_channel[1].roleid;
       } else {
-        Raid.role_id = "<@&" + feed_channel[1].roleid + ">";
+        Quest.role_id = "<@&" + feed_channel[1].roleid + ">";
       }
     }
 
     // DEFINE MORE VARIABLES
     Quest.Embed = feed_channel[1].embed ? feed_channel[1].embed : "quests.js";
 
-    switch (true) {
-      case (Channel.Geofences.indexOf("ALL") >= 0):
-      case (Channel.Geofences.indexOf(Quest.area.default) >= 0):
-      case (Channel.Geofences.indexOf(Quest.area.main) >= 0):
-      case (Channel.Geofences.indexOf(Quest.area.sub) >= 0):
+    let defGeo = (channel.geofences.indexOf(Quest.area.default) >= 0);
+    let mainGeo = (channel.geofences.indexOf(Quest.area.main) >= 0);
+    let subGeo = (channel.geofences.indexOf(Quest.area.sub) >= 0);
 
-        // REWARD FILTER
+    if (defGeo || mainGeo || subGeo) {
+
+      let rewardPass = (channel.Filter.Rewards.indexOf(Quest.quest_reward) >= 0 || channel.Filter.Rewards.indexOf(Quest.simple_reward) >= 0);
+      let typePass = (channel.Filter.Rewards.indexOf(Quest.reward_type) >= 0);
+
+      if (rewardPass || typePass) {
+
+        let Embed_Config = require(WDR.Dir + "/configs/embeds/" + Quest.Embed);
+
+        // VARIABLES
+        Quest.name = Quest.pokestop_name;
+        Quest.reward = Quest.quest_reward;
+        Quest.form = Quest.form_name;
+
+        // GET LOCATION INFO
+        Quest.lat = Quest.latitude;
+        Quest.lon = Quest.longitude;
+        Quest.area = Quest.area.embed;
+        Quest.url = Quest.pokestop_url;
+        Quest.map_url = WDR.Config.FRONTEND_URL;
+
+        // MAP LINK PROVIDERS
+        Quest.google = "[Google Maps](https://www.google.com/maps?q=" + Quest.latitude + "," + Quest.longitude + ")";
+        Quest.apple = "[Apple Maps](http://maps.apple.com/maps?daddr=" + Quest.latitude + "," + Quest.longitude + "&z=10&t=s&dirflg=d)";
+        Quest.waze = "[Waze](https://www.waze.com/ul?ll=" + Quest.latitude + "," + Quest.longitude + "&navigate=yes)";
+        Quest.pmsf = "[Scan Map](" + WDR.Config.FRONTEND_URL + "?lat=" + Quest.latitude + "&lon=" + Quest.longitude + "&zoom=15)";
+        Quest.rdm = "[Scan Map](" + WDR.Config.FRONTEND_URL + "@/" + Quest.latitude + "/" + Quest.longitude + "/15)";
+
+        Quest.reward_sprite = WDR.Get_Sprite(WDR, Quest);
+
+        Quest.marker_latitude = Quest.latitude + .0004;
+
+        Quest.body = await WDR.Generate_Tile(WDR, Quest, "quests", Quest.marker_latitude, Quest.lon, Quest.reward_sprite);
+        Quest.static_map = WDR.Config.STATIC_MAP_URL + 'staticmap/pregenerated/' + Quest.body;
+
+        // DECLARE VARIABLES
+        Quest.time = WDR.Time(null, "quest", Quest.Timezone);
+
+        // GET EMBED COLOR BASED ON QUEST DIFFICULTY
         switch (true) {
-          case (Channel.Filter.Rewards.indexOf(Quest.quest_reward) >= 0):
-          case (Channel.Filter.Rewards.indexOf(Quest.simple_reward) >= 0):
-          case (Channel.Filter.Rewards.indexOf("ALL") >= 0):
-
-            // PREPARE AND SEND TO DISCORDS
-            return Create_Quest_Embed(WDR, Channel, Quest);
+          case Quest.template.indexOf("easy") >= 0:
+            Quest.color = "00ff00";
+            break;
+          case Quest.template.indexOf("moderate") >= 0:
+            Quest.color = "ffff00";
+            break;
+          case Quest.template.indexOf("hard") >= 0:
+            Quest.color = "ff0000";
+            break;
+          default:
+            Quest.color = "00ccff";
         }
+
+        // CREATE QUEST EMBED
+        if (!Quest.sprite) {
+          Quest.sprite = Quest.url;
+        }
+
+        Quest.Embed = Embed_Config(WDR, Quest);
+
+        // SEND EMBED TO CHANNEL
+        return WDR.Send_Embed(WDR, Quest.Embed, channel.id);
+      }
     }
   }
 }
