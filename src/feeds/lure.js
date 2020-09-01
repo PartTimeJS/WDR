f612c508486e0218f7e474772f59510f01663927
Your Mother <r.g.levasseur@gmail.com>
2020-08-31 20:37:57 -0400

    Lure Feed Updates

--- src/feeds/lure.js
+++ src/feeds/lure.js
@@ -51,8 +51,6 @@ module.exports = async (WDR, LURE) => {
       }
     }
 
-    LURE.embed = feed_channel[1].embed ? feed_channel[1].embed : "lure.js";
-
     switch (true) {
       case (channel.Geofences.indexOf(LURE.area.default) >= 0):
       case (channel.Geofences.indexOf(LURE.area.main) >= 0):
@@ -62,89 +60,75 @@ module.exports = async (WDR, LURE) => {
           case (channel.Filter.Lure_Type.indexOf("ALL") >= 0):
           case (channel.Filter.Lure_Type.indexOf(LURE.type) >= 0):
 
-            Create_Lure_Embed(WDR, channel.id, LURE);
+            let Embed_Config = require(WDR.Dir + "/configs/embeds/" + (feed_channel[1].embed ? feed_channel[1].embed : "lure.js"));
+
+            let match = {};
+
+            match.color = "";
+
+            match.type = LURE.type;
+
+            match.name = LURE.name;
+            match.url = LURE.url ? LURE.url : "https://raw.githubusercontent.com/shindekokoro/PogoAssets/master/static_assets/png/Badge_Pokestop_SILVER_01.png";
+
+            match.time = WDR.Time(LURE.lure_expiration, "1", LURE.Timezone);
+            match.mins = Math.floor((LURE.lure_expiration - (LURE.Time_Now / 1000)) / 60);
+            match.secs = Math.floor((LURE.lure_expiration - (LURE.Time_Now / 1000)) - ((Math.floor((LURE.lure_expiration - (LURE.Time_Now / 1000)) / 60)) * 60));
+
+            match.lat = LURE.latitude;
+            match.lon = LURE.longitude;
+            match.area = LURE.area.embed;
+            match.map_url = WDR.Config.FRONTEND_URL;
+
+            match.google = "[Google Maps](https://www.google.com/maps?q=" + match.lat + "," + match.lon + ")";
+            match.apple = "[Apple Maps](http://maps.apple.com/maps?daddr=" + match.lat + "," + match.lon + "&z=10&t=s&dirflg=d)";
+            match.waze = "[Waze](https://www.waze.com/ul?ll=" + match.lat + "," + match.lon + "&navigate=yes)";
+            match.pmsf = "[Scan Map](" + WDR.Config.FRONTEND_URL + "?lat=" + match.lat + "&lon=" + match.lon + "&zoom=15)";
+            match.rdm = "[Scan Map](" + WDR.Config.FRONTEND_URL + "@/" + match.lat + "/" + match.lon + "/15)";
+
+            switch (match.type) {
+              case "Normal":
+                match.color = "ec78ea";
+                match.sprite = "https://raw.githubusercontent.com/ZeChrales/PogoAssets/master/static_assets/png/TroyKey.png";
+                break;
+              case "Glacial":
+                match.color = "5feafd";
+                match.sprite = "https://raw.githubusercontent.com/ZeChrales/PogoAssets/master/static_assets/png/TroyKey_glacial.png";
+                break;
+              case "Mossy":
+                match.color = "72ea38";
+                match.sprite = "https://raw.githubusercontent.com/ZeChrales/PogoAssets/master/static_assets/png/TroyKey_moss.png";
+                break;
+              case "Magnetic":
+                match.color = "fac036";
+                match.sprite = "https://raw.githubusercontent.com/ZeChrales/PogoAssets/00dd14bec9d3e17f89ddb021d71853c8b4667cf0/static_assets/png/TroyKey_magnetic.png"
+                break;
+              default:
+                match.color = "188ae2";
+                match.sprite = "https://raw.githubusercontent.com/ZeChrales/PogoAssets/master/static_assets/png/Badge_Pokestop_SILVER_01.png";
+                break;
+            }
+            if (match.mins >= 5) {
+
+              if (WDR.Config.LURE_PREGEN_TILES != "DISABLED") {
+                if (LURE.static_map) {
+                  match.static_map = LURE.static_map;
+                } else {
+                  match.body = await WDR.Generate_Tile(WDR, LURE, "lure", match.lat, match.lon, match.sprite);
+                  LURE.body = match.body;
+                  match.static_map = WDR.Config.STATIC_MAP_URL + 'staticmap/pregenerated/' + match.body;
+                  LURE.static_map = match.static_map;
+                }
+              }
+
+              match.embed = await Embed_Config(WDR, match);
+
+              return WDR.Send_Embed(WDR, match.embed, channel.id);
+            }
         }
     }
   }
 
   // END
   return;
-}
-
-
-async function Create_Lure_Embed(WDR, channel_id, LURE) {
-  let Embed_Config = require(WDR.Dir + "/configs/embeds/" + LURE.embed);
-
-  let match = {};
-
-  match.color = "";
-
-  match.name = LURE.name;
-  match.url = LURE.url ? LURE.url : "https://raw.githubusercontent.com/shindekokoro/PogoAssets/master/static_assets/png/Badge_Pokestop_SILVER_01.png";
-
-  match.time = WDR.Time(LURE.lure_expiration, "1", LURE.Timezone);
-  match.mins = Math.floor((LURE.lure_expiration - (LURE.Time_Now / 1000)) / 60);
-  match.secs = Math.floor((LURE.lure_expiration - (LURE.Time_Now / 1000)) - ((Math.floor((LURE.lure_expiration - (LURE.Time_Now / 1000)) / 60)) * 60));
-
-  match.lat = LURE.latitude;
-  match.lon = LURE.longitude;
-  match.area = LURE.area.embed;
-  match.map_url = WDR.Config.FRONTEND_URL;
-
-  match.google = "[Google Maps](https://www.google.com/maps?q=" + match.lat + "," + match.lon + ")";
-  match.apple = "[Apple Maps](http://maps.apple.com/maps?daddr=" + match.lat + "," + match.lon + "&z=10&t=s&dirflg=d)";
-  match.waze = "[Waze](https://www.waze.com/ul?ll=" + match.lat + "," + match.lon + "&navigate=yes)";
-  match.pmsf = "[Scan Map](" + WDR.Config.FRONTEND_URL + "?lat=" + match.lat + "&lon=" + match.lon + "&zoom=15)";
-  match.rdm = "[Scan Map](" + WDR.Config.FRONTEND_URL + "@/" + match.lat + "/" + match.lon + "/15)";
-
-  switch (LURE.type) {
-    case "Normal":
-      match.color = "ec78ea";
-      match.sprite = "https://raw.githubusercontent.com/ZeChrales/PogoAssets/master/static_assets/png/TroyKey.png";
-      break;
-    case "Glacial":
-      match.color = "5feafd";
-      match.sprite = "https://raw.githubusercontent.com/ZeChrales/PogoAssets/master/static_assets/png/TroyKey_glacial.png";
-      break;
-    case "Mossy":
-      match.color = "72ea38";
-      match.sprite = "https://raw.githubusercontent.com/ZeChrales/PogoAssets/master/static_assets/png/TroyKey_moss.png";
-      break;
-    case "Magnetic":
-      match.color = "fac036";
-      match.sprite = "https://raw.githubusercontent.com/ZeChrales/PogoAssets/00dd14bec9d3e17f89ddb021d71853c8b4667cf0/static_assets/png/TroyKey_magnetic.png"
-      break;
-    default:
-      match.color = "188ae2";
-      match.sprite = "https://raw.githubusercontent.com/ZeChrales/PogoAssets/master/static_assets/png/Badge_Pokestop_SILVER_01.png";
-      break;
-  }
-
-  match.marker_latitude = match.lat + .0004;
-
-  match.static_marker = [{
-      "url": "https://raw.githubusercontent.com/PartTimeJS/Assets/master/pogo/other/pokestop_near.png",
-      "height": 50,
-      "width": 50,
-      "x_offset": 0,
-      "y_offset": 0,
-      "latitude": match.marker_latitude,
-      "longitude": match.lon
-    },
-    {
-      "url": match.sprite,
-      "height": 40,
-      "width": 40,
-      "x_offset": 0,
-      "y_offset": -40,
-      "latitude": match.marker_latitude,
-      "longitude": match.lon
-    }
-  ];
-
-  match.static_map = WDR.Config.STATIC_MAP_URL + "&latitude=" + match.marker_latitude + "&longitude=" + match.lon + "&zoom=" + WDR.Config.STATIC_ZOOM + "&width=" + WDR.Config.STATIC_WIDTH + "&height=" + WDR.Config.STATIC_HEIGHT + "&scale=2&markers=" + encodeURIComponent(JSON.stringify(match.static_marker));
-
-  match.embed = await Embed_Config(WDR, match);
-
-  return WDR.Send_Embed(WDR, match.embed, channel_id);
 }
