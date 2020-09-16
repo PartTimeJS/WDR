c43f83b7cb6f20cd21e47b966a1c25e4db11e9ef
Your Mother <r.g.levasseur@gmail.com>
2020-09-16 14:12:41 -0400

    handle timeouts better

--- src/commands/subscription/pokemon/collect_option.js
+++ src/commands/subscription/pokemon/collect_option.js
@@ -1,93 +1,107 @@
 module.exports = (WDR, Functions, source, oMessage, bMessage, Member) => {
 
-  let BotMsg = bMessage;
-  let OriginalMsg = oMessage;
+    let BotMsg = bMessage;
+    let OriginalMsg = oMessage;
 
-  const filter = CollectedMsg => CollectedMsg.author.id == OriginalMsg.author.id;
-  const collector = OriginalMsg.channel.createMessageCollector(filter, {
-    time: 60000
-  });
+    const filter = CollectedMsg => CollectedMsg.author.id == OriginalMsg.author.id;
+    const collector = OriginalMsg.channel.createMessageCollector(filter, {
+        time: 60000
+    });
 
-  collector.on("collect", CollectedMsg => {
+    collector.on("collect", CollectedMsg => {
 
-    try {
-      CollectedMsg.delete();
-    } catch (e) {
+        try {
+            CollectedMsg.delete();
+        } catch (e) {
 
-    }
+        }
 
-    let input = CollectedMsg.content.split(" ").toString().toLowerCase();
+        let input = CollectedMsg.content.split(" ").toString().toLowerCase();
 
-    let adv_words = ["adv"],
-      add_words = ["ad", "add", "create"],
-      preset_words = ["preset", "presets"],
-      remove_words = ["remove", "rm"],
-      modify_words = ["change", "modify", "edit", "eidt"],
-      view_words = ["view", "vw", "veiw", "viw", "vew"],
-      pause_words = ["puase", "pasue", "pasue", "psaue", "paus", "pause"],
-      resume_words = ["resum", "rseume", "reusme", "resuem", "resume"];
-      
-    switch (true) {
-      case adv_words.some(word => input.includes(word)):
-        collector.stop("advanced");
-        break;
-      case add_words.some(word => input.includes(word)):
-        collector.stop("add");
-        break;
-      case preset_words.some(word => input.includes(word)):
-        collector.stop("preset");
-        break;
-      case remove_words.some(word => input.includes(word)):
-        collector.stop("remove");
-        break;
-      case modify_words.some(word => input.includes(word)):
-        collector.stop("edit");
-        break;
-      case view_words.some(word => input.includes(word)):
-        collector.stop("view");
-        break;
-      case pause_words.some(word => input.includes(word)):
-        collector.stop("pause");
-        break;
-      case resume_words.some(word => input.includes(word)):
-        collector.stop("resume");
-        break;
-      default:
-        collector.stop("cancel");
-    }
-  });
+        let adv_words = ["adv"],
+            add_words = ["ad", "add", "create"],
+            preset_words = ["preset", "presets"],
+            remove_words = ["remove", "rm"],
+            modify_words = ["change", "modify", "edit", "eidt"],
+            view_words = ["view", "vw", "veiw", "viw", "vew"],
+            pause_words = ["puase", "pasue", "pasue", "psaue", "paus", "pause"],
+            resume_words = ["resum", "rseume", "reusme", "resuem", "resume"];
+        
+        switch (true) {
+            case adv_words.some(word => input.includes(word)):
+                collector.stop("advanced"); break;
 
-  collector.on("end", (collected, arg) => {
+            case add_words.some(word => input.includes(word)):
+                collector.stop("add"); break;
 
-    try {
-      BotMsg.delete();
-    } catch (e) {
+            case preset_words.some(word => input.includes(word)):
+                collector.stop("preset"); break;
 
-    }
+            case remove_words.some(word => input.includes(word)):
+                collector.stop("remove"); break;
 
-    switch (arg) {
-      case "cancel":
-        return Functions.Cancel(WDR, Functions, OriginalMsg, Member);
-      case "advanced":
-        return Functions.Create(WDR, Functions, OriginalMsg, Member, true);
-      case "add":
-        return Functions.Create(WDR, Functions, OriginalMsg, Member, false);
-      case "preset":
-        return Functions.Preset(WDR, Functions, OriginalMsg, Member);
-      case "remove":
-        return Functions.Remove(WDR, Functions, OriginalMsg, Member);
-      case "edit":
-        return Functions.Modify(WDR, Functions, OriginalMsg, Member);
-      case "view":
-        return Functions.View(WDR, Functions, OriginalMsg, Member);
-      case "resume":
-      case "pause":
-        return Functions.Status(WDR, Functions, OriginalMsg, Member, arg);
-      case "time":
-        return Functions.TimedOut(WDR, Functions, OriginalMsg, Member);
-    }
-  });
+            case modify_words.some(word => input.includes(word)):
+                collector.stop("edit"); break;
 
-  // END
-  return;
+            case view_words.some(word => input.includes(word)):
+                collector.stop("view"); break;
+
+            case pause_words.some(word => input.includes(word)):
+                collector.stop("pause"); break;
+                
+            case resume_words.some(word => input.includes(word)):
+                collector.stop("resume"); break;
+
+            default:
+                collector.stop("cancel");
+        }
+    });
+
+    collector.on("end", (collected, arg) => {
+
+        try {
+            BotMsg.delete();
+        } catch (e) {
+
+        }
+
+        switch (arg) {
+            case "cancel":
+                return Functions.Cancel(WDR, Functions, OriginalMsg, Member);
+
+            case "advanced":
+                return Functions.Create(WDR, Functions, OriginalMsg, Member, true);
+
+            case "add":
+                return Functions.Create(WDR, Functions, OriginalMsg, Member, false);
+
+            case "preset":
+                return Functions.Preset(WDR, Functions, OriginalMsg, Member);
+
+            case "remove":
+                return Functions.Remove(WDR, Functions, OriginalMsg, Member);
+
+            case "edit":
+                return Functions.Modify(WDR, Functions, OriginalMsg, Member);
+
+            case "view":
+                return Functions.View(WDR, Functions, OriginalMsg, Member);
+
+            case "resume":
+            case "pause":
+                return Functions.Status(WDR, Functions, OriginalMsg, Member, arg);
+
+            case "time":
+                if (source != "complete") {
+                    Functions.TimedOut(WDR, Functions, OriginalMsg, Member);
+                }
+                return false;
+            
+            default: 
+                return;
+        }
+    });
+
+    // END
+    return;
 }
--- src/commands/subscription/pvp/collect_option.js
+++ src/commands/subscription/pvp/collect_option.js
@@ -1,93 +1,106 @@
 module.exports = (WDR, Functions, source, oMessage, bMessage, Member) => {
 
-  var BotMsg = bMessage;
-  let OriginalMsg = oMessage;
+    var BotMsg = bMessage;
+    let OriginalMsg = oMessage;
 
-  const filter = CollectedMsg => CollectedMsg.author.id == OriginalMsg.author.id;
-  const collector = OriginalMsg.channel.createMessageCollector(filter, {
-    time: 60000
-  });
+    const filter = CollectedMsg => CollectedMsg.author.id == OriginalMsg.author.id;
+    const collector = OriginalMsg.channel.createMessageCollector(filter, {
+        time: 60000
+    });
 
-  collector.on("collect", CollectedMsg => {
+    collector.on("collect", CollectedMsg => {
 
-    try {
-      CollectedMsg.delete();
-    } catch (e) {
+        try {
+            CollectedMsg.delete();
+        } catch (e) {
 
-    }
+        }
 
-    let input = CollectedMsg.content.split(" ")[0].toString().toLowerCase();
+        let input = CollectedMsg.content.split(" ")[0].toString().toLowerCase();
 
-    let add_words = ["ad", "add"],
-      preset_words = ["preset", "presets"],
-      remove_words = ["remove", "rm"],
-      modify_words = ["change", "modify", "edit", "eidt"],
-      view_words = ["view", "vw", "veiw", "viw", "vew"],
-      pause_words = ["puase", "pasue", "pasue", "psaue", "paus", "pause"],
-      resume_words = ["resum", "rseume", "reusme", "resuem", "resume"];
+        let add_words = ["ad", "add"],
+            preset_words = ["preset", "presets"],
+            remove_words = ["remove", "rm"],
+            modify_words = ["change", "modify", "edit", "eidt"],
+            view_words = ["view", "vw", "veiw", "viw", "vew"],
+            pause_words = ["puase", "pasue", "pasue", "psaue", "paus", "pause"],
+            resume_words = ["resum", "rseume", "reusme", "resuem", "resume"];
 
-    switch (true) {
-      case add_words.some(word => input.includes(word)):
-        collector.stop("add");
-        break;
-      case preset_words.some(word => input.includes(word)):
-        collector.stop("preset");
-        break;
-      case remove_words.some(word => input.includes(word)):
-        collector.stop("remove");
-        break;
-      case modify_words.some(word => input.includes(word)):
-        collector.stop("edit");
-        break;
-      case view_words.some(word => input.includes(word)):
-        collector.stop("view");
-        break;
-      case pause_words.some(word => input.includes(word)):
-        collector.stop("pause");
-        break;
-      case resume_words.some(word => input.includes(word)):
-        collector.stop("resume");
-        break;
-      default:
-        collector.stop("cancel");
-    }
-  });
+        switch (true) {
+            case add_words.some(word => input.includes(word)):
+                collector.stop("add"); break;
 
-  collector.on("end", (collected, arg) => {
+            case preset_words.some(word => input.includes(word)):
+                collector.stop("preset"); break;
 
-    if (BotMsg && BotMsg.channel.type != "dm") {
-      try {
-        BotMsg.delete();
-      } catch (e) {
+            case remove_words.some(word => input.includes(word)):
+                collector.stop("remove"); break;
 
-      }
-    }
+            case modify_words.some(word => input.includes(word)):
+                collector.stop("edit"); break;
 
-    switch (arg) {
-      case "cancel":
-        return Functions.Cancel(WDR, Functions, OriginalMsg, Member);
-      case "advanced":
-        return Functions.Create(WDR, Functions, OriginalMsg, Member, true);
-      case "add":
-        return Functions.Create(WDR, Functions, OriginalMsg, Member, false);
-      case "preset":
-        return Functions.Preset(WDR, Functions, OriginalMsg, Member, false);
-      case "remove":
-        return Functions.Remove(WDR, Functions, OriginalMsg, Member);
-      case "edit":
-        return Functions.Modify(WDR, Functions, OriginalMsg, Member);
-      case "view":
-        return Functions.View(WDR, Functions, OriginalMsg, Member);
-      case "resume":
-      case "pause":
-        return Functions.Status(WDR, Functions, OriginalMsg, Member, arg);
-      case "time":
-        return Functions.TimedOut(WDR, Functions, OriginalMsg, Member);
-      default:
-        return;
-    }
-  });
+            case view_words.some(word => input.includes(word)):
+                collector.stop("view"); break;
 
-  // END
-  return;
+            case pause_words.some(word => input.includes(word)):
+                collector.stop("pause"); break;
+
+            case resume_words.some(word => input.includes(word)):
+                collector.stop("resume"); break;
+                
+            default:
+                collector.stop("cancel");
+        }
+    });
+
+    collector.on("end", (collected, arg) => {
+
+        if (BotMsg && BotMsg.channel.type != "dm") {
+            try {
+                BotMsg.delete();
+            } catch (e) {
+
+            }
+        }
+
+        switch (arg) {
+            case "cancel":
+                return Functions.Cancel(WDR, Functions, OriginalMsg, Member);
+
+            case "advanced":
+                return Functions.Create(WDR, Functions, OriginalMsg, Member, true);
+
+
+            case "add":
+                return Functions.Create(WDR, Functions, OriginalMsg, Member, false);
+
+            case "preset":
+                return Functions.Preset(WDR, Functions, OriginalMsg, Member, false);
+
+            case "remove":
+                return Functions.Remove(WDR, Functions, OriginalMsg, Member);
+
+            case "edit":
+                return Functions.Modify(WDR, Functions, OriginalMsg, Member);
+
+            case "view":
+                return Functions.View(WDR, Functions, OriginalMsg, Member);
+
+            case "resume":
+            case "pause":
+                return Functions.Status(WDR, Functions, OriginalMsg, Member, arg);
+
+            case "time":
+                if (source != "complete") {
+                    Functions.TimedOut(WDR, Functions, OriginalMsg, Member);
+                }
+                return false;
+                
+            default:
+                return;
+        }
+    });
+
+    // END
+    return;
 }
