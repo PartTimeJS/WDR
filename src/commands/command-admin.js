0d63b0fbaa61a598e8a939e85f057e14203cb3b4
Your Mother <r.g.levasseur@gmail.com>
2020-09-17 08:25:44 -0400

    spacing

--- src/commands/command-admin.js
+++ src/commands/command-admin.js
@@ -1,11 +1,11 @@
 module.exports = (WDR, Message) => {
-    let command = Message.content.split(" ")[0].slice(1);
-    try {
-        if (WDR.Fs.existsSync(WDR.Dir + "/src/commands/admin/" + command.toLowerCase() + ".js")) {
-            let Cmd = require(WDR.Dir + "/src/commands/admin/" + command.toLowerCase() + ".js");
-            Cmd(WDR, Message);
-        }
-    } catch (error) {
-        
+  let command = Message.content.split(" ")[0].slice(1);
+  try {
+    if (WDR.Fs.existsSync(WDR.Dir + "/src/commands/admin/" + command.toLowerCase() + ".js")) {
+      let Cmd = require(WDR.Dir + "/src/commands/admin/" + command.toLowerCase() + ".js");
+      Cmd(WDR, Message);
     }
+  } catch (error) {
+
+  }
 }
