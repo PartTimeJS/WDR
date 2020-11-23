3a1cdd6642ce4847f4a60562ac9b056f3663f2ad
Your Mother <r.g.levasseur@gmail.com>
2020-11-23 10:21:06 -0500

    fix quest status commands

--- src/commands/subscription/quest/status.js
+++ src/commands/subscription/quest/status.js
@@ -1,18 +1,21 @@
 module.exports = (WDR, Functions, message, member, reason) => {
     WDR.wdrDB.query(`
-      SELECT
-        *
-      FROM
-        users
-      WHERE
-        user_id = '${member.id}'
-          AND 
-        guild_id = '${message.guild.id}
+        SELECT
+            *
+        FROM
+            wdr_users
+        WHERE
+            user_id = '${member.id}'
+                AND 
+            guild_id = '${message.guild.id}'
     ;`,
     function(error, user) {
         let change;
         if (error) {
-            return message.reply('There has been an error, please contact an Admin to fix.').then(m => m.delete(10000)).catch(console.error);
+            console.error(error);
+            return message.reply('There has been an error, please contact an Admin to fix.').then(m => m.delete({
+                timeout: 10000
+            }));
 
         } else if (!user || !user[0]) {
             WDR.Console.error(WDR, '[COMMANDS] [' + WDR.Time(null, 'stamp') + '] [quest.js/(subscription_status)] Could not retrieve user: ' + member.nickname + ' entry from dB.');
@@ -39,35 +42,56 @@ module.exports = (WDR, Functions, message, member, reason) => {
         } else {
             if (reason == 'pause') {
                 change = 0;
+                reason = 'PAUSED';
             }
             if (reason == 'resume') {
                 change = 1;
+                reason = 'ENABLED';
             }
 
             WDR.wdrDB.query(`
-            UPDATE
-              users
-            SET
-              quest_status = ${change}
-            WHERE
-              user_id = '${member.id}'
-                AND
-              guild_id = '${message.guild.id}';`,
+                UPDATE
+                    wdr_quest_subs
+                SET
+                    status = ${change}
+                WHERE
+                    user_id = '${member.id}'
+                        AND
+                    guild_id = '${message.guild.id}';
+            `,
             function(error) {
                 if (error) {
-                    return message.reply('There has been an error, please contact an Admin to fix.').then(m => m.delete(10000)).catch(console.error);
+                    return message.reply('There has been an error, please contact an Admin to fix.').then(m => m.delete({
+                        timeout: 10000
+                    }));
                 } else {
-                    let subscription_success = new WDR.DiscordJS.MessageEmbed().setColor('00ff00')
-                        .setAuthor(member.db.user_name, member.user.displayAvatarURL())
-                        .setTitle('Your Quest subscriptions have been set to `' + change + '`!')
-                        .setFooter('Saved to the Database.');
-                    return message.channel.send(subscription_success).then(m => m.delete({
-                        timeout: 5000
-                    })).catch(console.error);
+                    WDR.wdrDB.query(`
+                        UPDATE
+                            wdr_users
+                        SET
+                            quest_status = ${change}
+                        WHERE
+                            user_id = '${member.id}'
+                                AND
+                            guild_id = '${message.guild.id}';
+                    `,
+                    function(error) {
+                        if (error) {
+                            return message.reply('There has been an error, please contact an Admin to fix.').then(m => m.delete({
+                                timeout: 10000
+                            }));
+                        } else {
+                            let subscription_success = new WDR.DiscordJS.MessageEmbed().setColor('00ff00')
+                                .setAuthor(member.db.user_name, member.user.displayAvatarURL())
+                                .setTitle('Your Quest subscriptions have been set to `' + reason + '`!')
+                                .setFooter('Saved to the Database.');
+                            return message.channel.send(subscription_success).then(m => m.delete({
+                                timeout: 5000
+                            })).catch(console.error);
+                        }
+                    });
                 }
-            }
-            );
+            });
         }
-    }
-    );
+    });
 };
